7fe519f94815b6c18634a872bab22015e0b21062
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-05-24 14:35:31 +0300

    move to only REST APPLICATION (delete controllers for MVC part & delete templates)

--- src/main/java/com/stock/rest/AccountController.java
+++ src/main/java/com/stock/rest/AccountController.java
@@ -3,17 +3,22 @@ package com.stock.rest;
 import com.stock.dto.DepositDTO;
 import com.stock.dto.NewAccountDto;
 import com.stock.dto.accountDtos.AccountDto;
+import com.stock.exceptions.ImageNotFoundException;
 import com.stock.model.user.User;
 import com.stock.service.AccountService;
 import com.stock.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @RestController
@@ -52,4 +57,21 @@ public class AccountController {
         accountService.deleteAccountById(id);
         return ResponseEntity.ok().body(HttpStatus.OK);
     }
+
+//зробити цю функцію пізніше
+//    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    public String addPhoto(@AuthenticationPrincipal UserDetails userDetails,
+//                           @RequestParam("file") MultipartFile file,
+//                           Model model) {
+//        String email = userDetails.getUsername();
+//        User user = userService.getUserByEmail(email);
+//
+//        try {
+//            userService.saveUser(user, file);
+//        } catch (IOException e) {
+//            userService.saveUser(user);
+//            throw new ImageNotFoundException("Image not saved");
+//        }
+//        return "redirect:/api/v1/user";
+//    }
 }
